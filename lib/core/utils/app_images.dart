a9f6559dd398febdaf4bac6274f5ff558391cbc6
zyad mostafa <zyadmo026@gmail.com>
2025-02-06 23:10:29 +0200

    Add Inter font styles and image asset references

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAirpoddswhite
+  /// assets/images/airpoddswhite.svg
+  static const String imagesAirpoddswhite = "assets/images/airpoddswhite.svg";
+
+  /// Assets for imagesAirpodsblack
+  /// assets/images/airpodsblack.svg
+  static const String imagesAirpodsblack = "assets/images/airpodsblack.svg";
+
+  /// Assets for imagesAmex
+  /// assets/images/Amex.svg
+  static const String imagesAmex = "assets/images/Amex.svg";
+
+  /// Assets for imagesApplepay
+  /// assets/images/applepay.svg
+  static const String imagesApplepay = "assets/images/applepay.svg";
+
+  /// Assets for imagesCamera
+  /// assets/images/camera.svg
+  static const String imagesCamera = "assets/images/camera.svg";
+
+  /// Assets for imagesGooglmini
+  /// assets/images/googlmini.svg
+  static const String imagesGooglmini = "assets/images/googlmini.svg";
+
+  /// Assets for imagesGpay
+  /// assets/images/gpay.svg
+  static const String imagesGpay = "assets/images/gpay.svg";
+
+  /// Assets for imagesHeadphoneBlack
+  /// assets/images/headphoneBlack.svg
+  static const String imagesHeadphoneBlack = "assets/images/headphoneBlack.svg";
+
+  /// Assets for imagesHeadphonewhite
+  /// assets/images/headphonewhite.svg
+  static const String imagesHeadphonewhite = "assets/images/headphonewhite.svg";
+
+  /// Assets for imagesMastercard
+  /// assets/images/mastercard.svg
+  static const String imagesMastercard = "assets/images/mastercard.svg";
+
+  /// Assets for imagesMic2
+  /// assets/images/mic2.svg
+  static const String imagesMic2 = "assets/images/mic2.svg";
+
+  /// Assets for imagesMicrophone
+  /// assets/images/microphone.svg
+  static const String imagesMicrophone = "assets/images/microphone.svg";
+
+  /// Assets for imagesPaypal
+  /// assets/images/paypal.svg
+  static const String imagesPaypal = "assets/images/paypal.svg";
+
+  /// Assets for imagesVisa
+  /// assets/images/visa.svg
+  static const String imagesVisa = "assets/images/visa.svg";
+
+  /// Assets for imagesWatch
+  /// assets/images/watch.svg
+  static const String imagesWatch = "assets/images/watch.svg";
+}
+
---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static TextStyle stylesSemiBold10 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    fontSize: 10,
+  );
+  static TextStyle stylesMediam14 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+  static TextStyle stylesExtraBold14 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w800,
+    fontSize: 14,
+  );
+  static TextStyle stylesRegular10 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle stylesSemiBold20 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+  );
+  static TextStyle stylesMediam12 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+  );
+  static TextStyle stylesSemiBold9 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    fontSize: 9,
+  );
+  static TextStyle stylesRegular12 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  );
+  static TextStyle stylesBold14 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+  );
+  static TextStyle stylesRegular14 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+  static TextStyle stylesBold12 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+  );
+  static TextStyle stylesSemiBold32 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    fontSize: 32,
+  );
+  static TextStyle stylesRegular16 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+  static TextStyle stylesExtraBold20 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w800,
+    fontSize: 20,
+  );
+}
